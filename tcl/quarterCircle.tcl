f2c38b0abd99e5b035b79f70ae1a6dcd2042d744
xk2600 <xer0_kelvin@yahoo.com>
2019-02-26 17:17:03 -0600

    added some tcl goodness

---
+++ tcl/quarterCircle.tcl
@@ -0,0 +1,67 @@
+proc roundedCorner {r x y} {
+
+  #    x xmax   r
+  #    0   16  16
+  #  -16    0  16
+
+  set xmin $x
+  set ymin $y
+
+  set xmax [expr {$x + $r}]
+  set ymax [expr {$y + $r}]
+
+  set rmax [expr {pow($r,2)}]
+
+  set h [expr {$r/4}]
+
+  puts "\n\nr:$r->$rmax x:$x->$xmax y:$y->$ymax h:$h"
+
+  set result {}
+  set binres {}
+
+  while {$y <= $ymax} {
+    set x $xmin
+    set line {}
+    while {$x <= $xmax} {
+
+      set xySqrSum [expr { pow($x,2)+pow($y,2) }]
+      append line [expr {($xySqrSum <= $rmax) ? 1 : 0}]
+      incr x
+    }
+    append binres "$line\n"
+    incr y
+  }
+  puts \n\n$binres\n\n$result
+}
+
+proc bitbangArc {r} {
+
+  set rSqr [expr {pow($r,2)}]
+  set y 0
+  set x $r
+  
+  set linelen {}
+  
+  puts "\n\nr:$r rSqr:$rSqr x:$x y:$y"
+  
+  while {$y < $r} {
+    while { (pow($x,2)+pow($y,2)) >= $rSqr } {
+      puts "($x,$y):  x\u00B2+x\u00B2:[expr {(pow($x,2) + pow($y,2))}]    <=   r\u00B2:$rSqr"
+      incr x -1
+    } 
+    lappend linelen $x;
+    incr y
+  } 
+  
+  puts "\n\n \n\n"
+  foreach bits $linelen {
+  }
+  
+  return $linelen
+} 
+
+proc go {r} {
+  roundedCorner $r 0 0
+  bitbangArc $r
+}
+
